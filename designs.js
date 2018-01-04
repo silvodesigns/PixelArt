d5853ffe3b941fec8ae6f12c3e6a632b5ae532ff
Kevin Silvestre <keke1616@gmail.com>
2018-01-04 17:28:18 -0500

    Second Draft
    In this version of the application, the user is finally able to create a grid  for their canvas and start coloring

--- designs.js
+++ designs.js
@@ -7,30 +7,30 @@ const width = document.getElementById("input_width");
 // When size is submitted by the user, call makeGrid()
 const form = document.getElementById("sizePicker");
 
-form.addEventListener("submit",function makeGrid(e) {
-	// Your code goes here!
-		e.preventDefault();
-		const canvas = document.getElementById("pixel_canvas");
-		for (i = 0; i < height.value; i++) { 
-			const column= document.createElement("tr");
-			for(r = 0; r < width.value; r++){
-				const toAdd = document.createElement("td");
-				column.appendChild(toAdd);
-			}
-   		 	canvas.appendChild(column);
-		}
+form.addEventListener("submit", function makeGrid(e) {
+        // Your code goes here!
+        e.preventDefault();
+        const canvas = document.getElementById("pixel_canvas");
+        canvas.innerHTML = "";
+        for (i = 0; i < height.value; i++) {
+            const column = document.createElement("tr");
+            for (r = 0; r < width.value; r++) {
+                const toAdd = document.createElement("td");
+                column.appendChild(toAdd);
+            }
+            canvas.appendChild(column);
+        }
 
-		const area = document.getElementsByTagName("td");
-		for (i = 0; i < area.length; i++) {
-			area[i].addEventListener('click',function(){
-				this.style.background= color.value;
-			});
+        const area = document.getElementsByTagName("td");
+        //loop through every td and attach the following eventListener
+        for (i = 0; i < area.length; i++) {
+            area[i].addEventListener('click', function() {
+                this.style.background = color.value;
+            });
 
-			
-		}
 
-	}
-	
+        }
+
+    }
+
 );
-
-
